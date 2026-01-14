775e66b680f415378b0ad32bb4f1107384045d30
CodexSC <carlos.alexandur@gmail.com>
2026-01-14 13:56:59 +0100

    Changed file location. refactor: def and main()

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,6 +1,11 @@
-# Use function print
+#!/usr/bin/python3
 
-#Print "Programming is like building a multilingual puzzle"
+def print_message():
+    """
+    Prints the message: "Programming is like building a multilingual puzzle"
+    """
+    print("Programming is like building a multilingual puzzle")
 
-print("Programming is like building a multilingual puzzle")
+if __name__ == "__main__":
+    print_message()
 
