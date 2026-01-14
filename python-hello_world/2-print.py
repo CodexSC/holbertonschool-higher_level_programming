002c6d0e6e2a473048500cf1754f5a36c4cec8e6
CodexSC <carlos.alexandur@gmail.com>
2026-01-14 14:08:40 +0100

    refactor: 2-print.py for checker

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-
-def print_message():
-    """
-    Prints the message: "Programming is like building a multilingual puzzle"
-    """
-    print("Programming is like building a multilingual puzzle\n")
-
-if __name__ == "__main__":
-    print_message()
+print("\"Programming is like building a multilingual puzzle")
 
