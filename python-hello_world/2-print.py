155b3d8d1d12c96523d40f5622a34ac93b28bb9f
CodexSC <carlos.alexandur@gmail.com>
2026-01-14 13:58:22 +0100

    fix: new line character

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -4,7 +4,7 @@ def print_message():
     """
     Prints the message: "Programming is like building a multilingual puzzle"
     """
-    print("Programming is like building a multilingual puzzle")
+    print("Programming is like building a multilingual puzzle\n")
 
 if __name__ == "__main__":
     print_message()
