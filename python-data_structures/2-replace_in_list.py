e5fd4d59b94e8b71ac34f480974d3d085ce6f8bf
CodexSC <carlos.alexandur@gmail.com>
2026-01-21 14:55:13 +0100

    fixed pycodestyle warnings

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,6 +3,8 @@
 Function that replaces an element of a list at a
 specific position without modifying the original list.
 """
+
+
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
