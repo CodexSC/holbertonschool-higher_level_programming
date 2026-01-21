042759a1f99e38ff4a0600949b0e64f3fdedf656
CodexSC <carlos.alexandur@gmail.com>
2026-01-21 15:08:08 +0100

    several issues fixed for testing

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 """
-Find the max integer in a list
+Find the biggest integer of a list
 """
 
+
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
+    if not my_list:
         return None
-    else:
-        return max(my_list)
+
+    max_value = my_list[0]
+    for num in my_list:
+        if num > max_value:
+            max_value = num
+
+    return max_value
