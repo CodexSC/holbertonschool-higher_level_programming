2bf4bc0a7b27d42c0f49ac5bd50e2755173626d0
CodexSC <carlos.alexandur@gmail.com>
2026-01-21 15:06:24 +0100

    fixed: errors to test with checker

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """
-def no_c(my_string):
-    return my_string.translate({ord(c): None for c in 'cC'})
+Remove all characters c and C from a string
 """
 
+
 def no_c(my_string):
-    return my_string.translate({ord(c): None for c in 'cC'})
+    new_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
