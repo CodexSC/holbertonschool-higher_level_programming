10fa32c26b3ed4178f27489a5b3328da07439240
CodexSC <carlos.alexandur@gmail.com>
2026-01-21 14:58:09 +0100

    solved pycodestyle warnings

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -4,6 +4,7 @@ Function that replaces an element in a list at a specific position
 without modifying the original list
 """
 
+
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
