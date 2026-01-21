f83084869fd3997e25898ef9b9bd6f98dc63e3dc
CodexSC <carlos.alexandur@gmail.com>
2026-01-21 15:03:30 +0100

    fixed function issues, several

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -5,4 +5,9 @@ Add two tuples
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    a1 = tuple_a[0] if len(tuple_a) >= 1 else 0
+    a2 = tuple_a[1] if len(tuple_a) >= 2 else 0
+    b1 = tuple_b[0] if len(tuple_b) >= 1 else 0
+    b2 = tuple_b[1] if len(tuple_b) >= 2 else 0
+
+    return (a1 + b1, a2 + b2)
