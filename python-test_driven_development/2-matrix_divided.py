bb9c4c842e0e66cd8af54ae64ed2657d3c8dfbd1
CodexSC <carlos.alexandur@gmail.com>
2026-01-22 15:49:39 +0100

    implemented matrix divided function and tester

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module for matrix division."""
+
+def matrix_divided(matrix, div):
+    """Divide matrix elements by div."""
+    if type(matrix) is not list:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if len(matrix) == 0:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if type(matrix[0]) is not list:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if len(matrix[0]) == 0:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    for row in matrix:
+        if len(row) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+        for element in row:
+            if type(element) is not int and type(element) is not float:
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    return [[round(element / div, 2) for element in row] for row in matrix]
