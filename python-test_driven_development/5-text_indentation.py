01eb979b4f4e275b7ec84659494584451680def7
CodexSC <carlos.alexandur@gmail.com>
2026-01-23 16:21:54 +0100

    rollback

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-"""Prints a text with 2 new lines after each of these characters: ., ? and :."""
+"""
+Function that prints a text with 2 new lines
+after each of these characters: ., ? and :.
+"""
 
 
 def text_indentation(text):
-    """Prints a text with 2 new lines after each of these characters: ., ? and :."""
+    """
+    Function that prints a text with 2 new lines after
+    each of these characters: ., ? and :.
+    """
     if type(text) is not str:
         raise TypeError("text must be a string")
     text = text.replace(".", ".\n\n")
