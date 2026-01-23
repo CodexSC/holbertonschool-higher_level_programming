ebb78f2b35fd3b685a15f32cd5e496c448588221
CodexSC <carlos.alexandur@gmail.com>
2026-01-23 12:45:39 +0100

    edited doctring statement

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -5,6 +5,9 @@ Prints a square with the character #.
 
 
 def print_square(size):
+    """
+    Prints a square with the character #.
+    """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
