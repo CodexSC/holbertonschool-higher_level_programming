36867466db8503ec1ddd87528b1c57a555d6b7e2
CodexSC <carlos.alexandur@gmail.com>
2026-01-23 12:09:26 +0100

    changes to remove trailspace

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,16 +3,13 @@
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints a name.
-
-    Args:
-        first_name (str): The first name to print.
-        last_name (str): The last name to print.
-    Raises:
-        TypeError: If either of first_name or last_name are not strings.
-    """
+    """Prints a name."""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+
+    name = "My name is {}".format(first_name)
+    if last_name:
+        name += " {}".format(last_name)
+    print(name)
