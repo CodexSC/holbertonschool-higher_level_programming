d419243d677d924493e928eb49a2a52f23c76997
CodexSC <carlos.alexandur@gmail.com>
2026-01-23 12:11:55 +0100

    refactor: for testing purposes

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""Module that contains a function that prints a name."""
+"""
+Function that prints My name is <first name> <last name>
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints a name."""
-    if not isinstance(first_name, str):
+    """Prints My name is <first name> <last name>"""
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-
-    name = "My name is {}".format(first_name)
-    if last_name:
-        name += " {}".format(last_name)
-    print(name)
+    print("My name is {} {}".format(first_name, last_name))
