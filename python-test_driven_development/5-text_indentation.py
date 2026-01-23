9d45ce9020b84c3f8727e2c576c94e78db3988bd
CodexSC <carlos.alexandur@gmail.com>
2026-01-23 16:05:34 +0100

    pycodestyle fixes

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,7 +3,10 @@
 
 
 def text_indentation(text):
-    """Prints a text with 2 new lines after each of these characters: ., ? and :"""
+    """
+    Prints a text with 2 new lines
+    after each of these characters: ., ? and :
+    """
     if type(text) is not str:
         raise TypeError("text must be a string")
 
