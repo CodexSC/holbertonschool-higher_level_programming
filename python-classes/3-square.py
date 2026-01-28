9797035c506b20da4d6a9721a0d615f1913756a4
CodexSC <carlos.alexandur@gmail.com>
2026-01-28 10:46:47 +0100

    implemented 3-square

---
+++ python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Defines a square by: (based on 0-square.py)
+Private instance attribute: size
+"""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """
+        Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        # Store size as a private attribute (name mangling with __)
+        self.__size = size
