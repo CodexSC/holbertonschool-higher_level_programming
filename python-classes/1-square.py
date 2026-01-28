7d1c2f83facc7dbcf738c5efea59769834bb1b1d
CodexSC <carlos.alexandur@gmail.com>
2026-01-28 10:20:11 +0100

    README

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Defines a square by: (based on 0-square.py)
+Private instance attribute: size
+"""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
