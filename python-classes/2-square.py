f40c80f4fc099e542ff0489692e4f298e82260ae
CodexSC <carlos.alexandur@gmail.com>
2026-01-28 10:28:51 +0100

    implemented square with size attributes

---
+++ python-classes/2-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+
+"""
+Defines a square by: (based on 0-square.py)
+Private instance attribute: size
+"""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
