ca86eeb5ca862c70a2c63dbda305f1c93fb966d2
CodexSC <carlos.alexandur@gmail.com>
2026-01-28 10:34:28 +0100

    code changes and documentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -15,5 +15,18 @@ class Square:
 
         Args:
             size (int): The size of the new square.
+
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
         """
+        #Validate that size is an integer
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    #Stores size as a private attribute (name mangling with __)
         self.__size = size
