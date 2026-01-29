ebdea4209062a4e77cfb540cbd463455bc91ae50
CodexSC <carlos.alexandur@gmail.com>
2026-01-29 15:38:47 +0100

    syntax corrections and ordered the modules

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -43,32 +43,27 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-        return self.__size
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def my_print(self):
         """Print the square with the # character."""
-        for i in range(0, self.__size):
-            [print("#", end="") for j in range(self.__size)]
-            print("")
-
         if self.__size == 0:
             print("")
+            return
 
-        return
-
-    def position(self):
-        """Print the square with the # character."""
+        [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
-            [print("#", end="") for j in range(self.__size)]
-            print("")
-
-        if self.__size == 0:
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
             print("")
 
         return
 
-    def area(self):
-        """Return the current area of the square."""
-        return self.__size * self.__size
 
-        return
