ac6a123cf6bab6941e1195cd39bfc4ffe899dae3
CodexSC <carlos.alexandur@gmail.com>
2026-01-29 15:23:15 +0100

    added functions to module

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -50,3 +50,8 @@ class Square:
         for i in range(0, self.__size):
             [print("#", end="") for j in range(self.__size)]
             print("")
+
+        if self.__size == 0:
+            print("")
+
+        return
