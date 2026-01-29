eec59d0930b83734b1114ab064c6c0715023eaed
CodexSC <carlos.alexandur@gmail.com>
2026-01-29 15:21:20 +0100

    implemented print suqare with # module

---
+++ python-classes/5-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+Defines a square by: (based on 0-square.py)
+Private instance attribute: size
+"""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """
+        Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        # Store size as a private attribute (name mangling with __)
+        self.__size = size
+
+    def area(self):
+        """Return the current area of the square."""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+        return self.__size
+
+    def my_print(self):
+        """Print the square with the # character."""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
