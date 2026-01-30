2457fc8908cf60efd599b34c5a59e62d3e66b542
CodexSC <carlos.alexandur@gmail.com>
2026-01-30 10:21:52 +0100

    several changes in code structure

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -6,30 +6,18 @@ class Rectangle:
     """Represents a rectangle."""
     pass
 
-@property
-def width(self):
-    return self.__width
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
 
-@width.setter
-def width(self, value):
-    if not isinstance(value, int):
-        raise TypeError("width must be an integer")
-    if value < 0:
-        raise ValueError("width must be >= 0")
-    self.__width = value
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
 
-@property
-def height(self):
-    return self.__height
-
-@height.setter
-def height(self, value):
-    if not isinstance(value, int):
-        raise TypeError("height must be an integer")
-    if value < 0:
-        raise ValueError("height must be >= 0")
-    self.__height = value
-
-def __init__(self, width=0, height=0):
-    self.width = width
-    self.height = height
+        self.__width = width
+        self.__height = height
