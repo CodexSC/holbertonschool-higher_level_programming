5df902e796f869946c2da4c27b444a65edde068b
CodexSC <carlos.alexandur@gmail.com>
2026-02-05 10:31:01 +0100

    changed syntax

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-"""inherits_from module"""
+"""Module that contains inherits_from function"""
 
 
 def inherits_from(obj, a_class):
-    return issubclass(type(obj), a_class)
+    """Check if obj is instance of a class that inherited from a_class"""
+    try:
+        return issubclass(type(obj), a_class)
+    except TypeError:
+        return False
