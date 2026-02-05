46a39d58ed3c5436ac34e92f7ccc99386ab8a538
CodexSC <carlos.alexandur@gmail.com>
2026-02-05 10:27:02 +0100

    syntax changes

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@ def inherits_from(obj, a_class):
     """ Returns True if the object is an instance of a class that inherited
         (directly or indirectly) from the specified class ; otherwise False.
     """
-    return(obj is not a class and issubclass(type(obj), a_class))
+    return issubclass(type(obj), a_class)
