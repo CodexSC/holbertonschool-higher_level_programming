86ad861d99661886fe50dc10bc6e38424e586bf4
CodexSC <carlos.alexandur@gmail.com>
2026-02-06 12:17:20 +0100

    Changes in syntax

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
-"""Class Rectangle"""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-Rectangle = __import__('9-rectangle').Rectangle
+"""Defines a Square class that inherits from Rectangle."""
+
+Rectangle = __import__('8-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class that inherits from Rectangle."""
+
+    def __init__(self, size):
+        """Initialize a Square.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
