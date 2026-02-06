bb8985c402c8e048647d85036e058e95d8cada96
CodexSC <carlos.alexandur@gmail.com>
2026-02-06 12:34:28 +0100

    Further changes in code structure

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
-"""Defines a base geometry class BaseGeometry."""
+"""Defines a BaseGeometry class."""
 
 
 class BaseGeometry:
-    """Represent base geometry."""
+    """Base class for geometry objects."""
 
     def area(self):
-        """Not yet implemented."""
+        """
+        Raises an Exception indicating area is not implemented.
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate a parameter as an integer.
+        """
+        Validates that value is a positive integer.
 
         Args:
-            name (str): The name of the parameter.
-            value (int): The parameter to validate.
+            name (str): Name of the value
+            value (int): Value to validate
+
         Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
+            TypeError: If value is not an integer
+            ValueError: If value <= 0
         """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
+
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+
