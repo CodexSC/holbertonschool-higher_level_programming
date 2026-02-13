a145537924d57d1b7fec8f2340ca7b554b6c78ec
CodexSC <carlos.alexandur@gmail.com>
2026-02-13 10:13:37 +0100

    docstring and pycodestyle corrections

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""
-Write a function that appends a string at the end of a text file
-and returns the number of characters added
-"""
+"""Module for appending text to a file."""
 
 
 def append_write(filename="", text=""):
+    """Appends a string to a text file and returns the number of chars added."""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
