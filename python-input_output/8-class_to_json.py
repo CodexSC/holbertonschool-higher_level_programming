05b3925219ce04d70d36a8132cd84015f9c44b76
CodexSC <carlos.alexandur@gmail.com>
2026-02-13 15:23:28 +0100

    Pycodestyle changes

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -3,5 +3,7 @@
 
 
 def class_to_json(obj):
-    """Returns the dictionary description with simple data structure for JSON serialization of an object."""
-    return {key: value for key, value in obj.__dict__.items() if isinstance(value, (list, dict, str, int, bool))}
+    """Returns the dictionary description with simple data structure
+    for JSON serialization of an object."""
+    return {key: value for key, value in obj.__dict__.items()
+            if isinstance(value, (list, dict, str, int, bool))}
