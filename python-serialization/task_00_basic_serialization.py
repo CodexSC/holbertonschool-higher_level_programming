94db7c5a3b07eba93682eaa49f870ebcd6c275db
CodexSC <carlos.alexandur@gmail.com>
2026-02-13 19:02:31 +0100

    added missing serial/ functions

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""Function that writes an object to a text file using JSON representation."""
+import json
 
 
 def save_to_json_file(my_obj, filename):
-    import json
-    with open(filename, 'w') as f:
+    """Writes an object to a text file using JSON representation.
+
+    Args:
+        my_obj: The object to save.
+        filename (str): The name of the file to write to.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
         json.dump(my_obj, f)
