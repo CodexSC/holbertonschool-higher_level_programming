16eb2c75056fbb382134fb80ab10126ec3f764ee
CodexSC <carlos.alexandur@gmail.com>
2026-02-13 10:30:30 +0100

    deleted file, changed module format

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 """Function that returns an object (Python data structure) represented
 by a json string"""
-
-
 import json
 
 
 def from_json_string(my_str):
-    """Function that returns an object (Python data structure) represented
-    by a json string"""
+    """Returns a Python data structure by a JSON string"""
     return json.loads(my_str)
