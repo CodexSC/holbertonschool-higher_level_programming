4ce1f7c8d2d16e6b79c80d4760a4e42f8b27e839
CodexSC <carlos.alexandur@gmail.com>
2026-02-13 15:20:21 +0100

    added functions to module

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -6,3 +6,10 @@ import json
 def class_to_json(obj):
     """Class to JSON function."""
     return obj.__dict__
+
+if __name__ == "__main__":
+    my_list = []
+    my_list.append(1)
+    my_list.append(2)
+    my_list.append(3)
+    print(class_to_json(my_list))
