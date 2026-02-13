3f4e12c74e58d84dc1f38920a59b128bfc248267
CodexSC <carlos.alexandur@gmail.com>
2026-02-13 19:28:59 +0100

    syntax fixes for checker

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-"""
-Write a function that returns the dictionary description
-with simple data structure (list, dictionary, string,
-integer and boolean) for JSON serialization of an object
-"""
-import json
+"""Script that adds command line arguments to a Python list
+saved in a JSON file."""
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
