958f211b8333dfd7cffc66310f78ee04142b9138
CodexSC <carlos.alexandur@gmail.com>
2026-03-06 23:35:38 +0100

    Refactor database connection string format in 11-model_state_insert.py for improved readability

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+"""Adds the State object Louisiana to the database hbtn_0e_6_usa"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-
 
 if __name__ == "__main__":
     username = sys.argv[1]
@@ -11,8 +11,9 @@ if __name__ == "__main__":
     database = sys.argv[3]
 
     engine = create_engine(
-        f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}",
-        pool_pre_ping=True
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            username, password, database
+        )
     )
 
     Session = sessionmaker(bind=engine)
