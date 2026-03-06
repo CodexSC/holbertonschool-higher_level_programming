ffdecba656680963bccb99c9156225e72fd6e8e6
CodexSC <carlos.alexandur@gmail.com>
2026-03-06 23:44:06 +0100

    Refactor code for improved readability and consistency in 12-model_state_update_id_2.py

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
+"""Changes the name of the State object with id = 2
+to 'New Mexico' in the database hbtn_0e_6_usa"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
     engine = create_engine(
-        f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}",
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
         pool_pre_ping=True
     )
-
     Session = sessionmaker(bind=engine)
     session = Session()
-
     state = session.query(State).filter(State.id == 2).first()
-
     if state:
         state.name = "New Mexico"
         session.commit()
-
     session.close()
