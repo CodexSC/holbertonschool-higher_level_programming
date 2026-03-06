7294671b5bce1610832de5f6f1e905c1cf22c6fb
CodexSC <carlos.alexandur@gmail.com>
2026-03-06 23:57:11 +0100

    Refactor 1-filter_states.py to improve code clarity and filter states starting with 'N'

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
+"""Lists all states with a name starting with N from hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=username,
-        passwd=password,
-        db=database
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
-
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
-
-    states = cursor.fetchall()
-
-    for state in states:
-        print(state)
-
-    cursor.close()
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    )
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
     db.close()
