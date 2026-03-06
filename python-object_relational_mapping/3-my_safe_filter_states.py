4b1d9c69d6c166c07adfb7ac56b52fd73f19a6e1
CodexSC <carlos.alexandur@gmail.com>
2026-03-06 22:47:47 +0100

    Add script to safely display all values in the states table matching a given name

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Displays all values in the states table where name matches the argument (safe from SQL injection)"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = db.cursor()
+    state_name = sys.argv[4]
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cur.execute(query, (state_name,))
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    db.close()
