a2282e87ec3926d86257af20f3ff88b6163f55ce
CodexSC <carlos.alexandur@gmail.com>
2026-03-06 23:43:24 +0100

    Refactor database connection string format for improved readability in 12-model_state_update_id_2.py

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,22 +1,27 @@
-Two E501s to fix, plus the usual pool_pre_ping and missing port:
-python#!/usr/bin/python3
-"""Changes the name of the State object with id = 2
-to 'New Mexico' in the database hbtn_0e_6_usa"""
+#!/usr/bin/python3
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        )
+        f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}",
+        pool_pre_ping=True
     )
+
     Session = sessionmaker(bind=engine)
     session = Session()
+
     state = session.query(State).filter(State.id == 2).first()
+
     if state:
         state.name = "New Mexico"
         session.commit()
+
     session.close()
