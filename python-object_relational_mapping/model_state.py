066261cc443bf620fc024fcc0011ea56e83970f3
CodexSC <carlos.alexandur@gmail.com>
2026-03-06 22:50:25 +0100

    Add State class definition for ORM with SQLAlchemy

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines State class and Base instance for ORM"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
