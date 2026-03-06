6da582359316a506c74828583d83253836c333eb
CodexSC <carlos.alexandur@gmail.com>
2026-03-06 16:21:30 +0100

    Fix SQL script to create user_0d_1 and grant all privileges, removing unnecessary ALTER USER command

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
--- Creates or updates user_0d_1 with full server privileges
+-- Creates user_0d_1 and grants it all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
