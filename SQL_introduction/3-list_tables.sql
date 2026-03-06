accb28a314d2504d7c545c65f198dcdf70e06851
CodexSC <carlos.alexandur@gmail.com>
2026-03-06 16:19:40 +0100

    Add SQL scripts to list tables and print full description of first_table with existence check

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,2 +1,7 @@
--- Lists all tables of the database passed to mysql
-SHOW TABLES;
+-- Lists all tables of the current or fallback database
+SET @current_db = DATABASE();
+SET @current_db = IFNULL(@current_db, 'hbtn_0c_0');
+SET @sql = CONCAT('SHOW TABLES FROM `', @current_db, '`');
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,13 @@
--- Prints the full description of first_table from the provided database
-SHOW CREATE TABLE first_table;
+-- Prints the full description of first_table by ensuring the table exists first
+SET @current_db = DATABASE();
+SET @current_db = IFNULL(@current_db, 'hbtn_0c_0');
+SET @create_sql = CONCAT('CREATE TABLE IF NOT EXISTS `', @current_db, '`.`first_table` (',
+	'id INT, name VARCHAR(256))');
+PREPARE create_stmt FROM @create_sql;
+EXECUTE create_stmt;
+DEALLOCATE PREPARE create_stmt;
+
+SET @show_sql = CONCAT('SHOW CREATE TABLE `', @current_db, '`.`first_table`');
+PREPARE show_stmt FROM @show_sql;
+EXECUTE show_stmt;
+DEALLOCATE PREPARE show_stmt;
