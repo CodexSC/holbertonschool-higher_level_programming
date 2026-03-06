781c699f40f92a966bac8f9438befe6a8752125e
CodexSC <carlos.alexandur@gmail.com>
2026-03-06 23:41:57 +0100

    Fix E501 line length issues and update database connection string format in 12-model_state_update_id_2.py

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,5 +1,7 @@
-#!/usr/bin/python3
-"""Changes the name of the State object with id = 2 to 'New Mexico' in the database hbtn_0e_6_usa"""
+Two E501s to fix, plus the usual pool_pre_ping and missing port:
+python#!/usr/bin/python3
+"""Changes the name of the State object with id = 2
+to 'New Mexico' in the database hbtn_0e_6_usa"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -7,8 +9,9 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        )
     )
     Session = sessionmaker(bind=engine)
     session = Session()
